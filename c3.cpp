97e66a252a6cce5b0f6612f4c7509bb60435272a
online2002 <onlinereinvention@gmail.com>
2023-08-30 17:39:20 +0530

    adding comments and changing variable names

--- c3.cpp
+++ c3.cpp
@@ -1,73 +1,81 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string dir,com;
-    char id,ifc;
+    //variables declaration...
+    string direction,commands;
+    char initial_direction,initial_facing;
     int cx,cy,cz,r=0,l=0;
-    cout<<"Enter Initial Direction : ";
-    cin>>id;
-    ifc=id;
-    if(id=='n')dir="nesw";
-    if(id=='e')dir="eswn";
-    if(id=='s')dir="swne";
-    if(id=='w')dir="wnes";
+
+    //taking inputs...
+    cout<<"Enter Initial direction : ";
+    cin>>initial_direction;
     cout<<"Enter Co-ordinates (x,y,z) : ";
     cin>>cx>>cy>>cz;
+    cout<<"Enter command as String:";
+    cin>>commands;
 
-    cout<<"Enter Command as String:";
-    cin>>com;
-    for(int i=0;i<com.length();i++){
-        switch(com[i]){
+    //stablizing inputs...
+    initial_facing=initial_direction;
+    if(initial_direction=='n')direction="nesw";
+    if(initial_direction=='e')direction="eswn";
+    if(initial_direction=='s')direction="swne";
+    if(initial_direction=='w')direction="wnes";
+
+
+    //main logic...
+    for(int i=0;i<commands.length();i++){
+        //performing each operations...
+        switch(commands[i]){
             case 'u':
-                ifc = 'u';    
+                initial_facing = 'u';    
                 break;
             case 'd':
-                ifc = 'd';
+                initial_facing = 'd';
                 break;
             case 'r':
                 r++;
                 if(r>3)r=0;
                 l--;
                 if(l<0)l=3;
-                id = dir[r]; 
-                ifc = id;
+                initial_direction = direction[r]; 
+                initial_facing = initial_direction;
                 break;
             case 'l':
                 l++;
                 if(l>3)l=0;
                 r--;
                 if(r<0)r=3;
-                id = dir[l];
-                ifc = id; 
+                initial_direction = direction[l];
+                initial_facing = initial_direction; 
                 break;
             case 'f':
-                if(ifc == 'u'){
+                if(initial_facing == 'u'){
                     cz++;break;
                 }
-                if(ifc == 'd'){
+                if(initial_facing == 'd'){
                     cz--;break;
                 }
-                if(id == dir[0])cy++;
-                if(id == dir[2])cy--;
-                if(id == dir[1])cx++;
-                if(id == dir[3])cx--;
+                if(initial_direction == direction[0])cy++;
+                if(initial_direction == direction[2])cy--;
+                if(initial_direction == direction[1])cx++;
+                if(initial_direction == direction[3])cx--;
                 break;
             case 'b':
-                if(ifc == 'u'){
+                if(initial_facing == 'u'){
                     cz--;break;
                 }
-                if(ifc == 'd'){
+                if(initial_facing == 'd'){
                     cz++;break;
                 }
-                if(id == dir[0])cy--;
-                if(id == dir[2])cy++;
-                if(id == dir[1])cx--;
-                if(id == dir[3])cx++;
+                if(initial_direction == direction[0])cy--;
+                if(initial_direction == direction[2])cy++;
+                if(initial_direction == direction[1])cx--;
+                if(initial_direction == direction[3])cx++;
                 break;
         }
-        cout<<"\n"<<com[i]<<" - ("<<cx<<", "<<cy<<", "<<cz<<") - "<<ifc;
+        cout<<"\n"<<commands[i]<<" - ("<<cx<<", "<<cy<<", "<<cz<<") - "<<initial_facing;
     }
     cout<<"\nfinal co-ordinates : "<<"("<<cx<<", "<<cy<<", "<<cz<<")";
-    cout<<"\nfinal direction : "<<id;
+    cout<<"\nfinal direction : "<<initial_direction;
     return 0;
 }
